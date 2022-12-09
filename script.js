ef275e29e4d0b9814b3365bff0f93452a15e7807
Ric Cabrera <42082247+cartonioh@users.noreply.github.com>
2022-12-09 22:08:15 +0000

    Add active state to navbar buttons

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
+const NAV_DEMO_BTN = "nav-demo-btn";
+const NAV_WIDGET_BTN = "nav-widget-btn";
+const BTN_OUTLINED_ACTIVE = "button-outlined-active";
+const LN_ADDRESS_TEXT = "lightning-address-text";
+
 const widgetsView = document.getElementById("view-widgets");
 const demosView = document.getElementById("view-demos");
 
 // Start out with demos hidden
 demosView.style.display = "none";
 
-function onClickDemos() {
+/** Handles user clicking on demo button  **/
+function onClickDemos(e) {
+  // Set visibility
   demosView.style.display = "flex";
   widgetsView.style.display = "none";
+
+  // Scroll to top
   window.scrollTo(0, 0);
+  e.classList.add(BTN_OUTLINED_ACTIVE);
+  
+  // Set to active
+  document.getElementById(NAV_WIDGET_BTN).classList.remove(BTN_OUTLINED_ACTIVE);
 }
 
-function onClickWidgets() {
+/** Handles user clicking on widgets button  **/
+function onClickWidgets(e) {
+  // Set visibility
   widgetsView.style.display = "flex";
   demosView.style.display = "none";
+
+  // Scroll to top
   window.scrollTo(0, 0);
+  
+  // Set to active
+  e.classList.add(BTN_OUTLINED_ACTIVE);
+document.getElementById(NAV_DEMO_BTN).classList.remove(BTN_OUTLINED_ACTIVE);
 }
 
+/** Copies lightning address with a notification **/
 function copyLightningAddress(text) {
-  const el = document.getElementById("lightning-address-text");
+  const el = document.getElementById(LN_ADDRESS_TEXT);
   navigator.clipboard.writeText(el.textContent)
     .then(() => toastr.success("Copied lightning address"))
     .catch(e => toastr.error("Could not copy lightning address"))
 }
 
 // Set toastr options
-toastr.options = { ...toastr.options, positionClass: "toast-bottom-center" };
+toastr.options = { 
+  ...toastr.options, 
+  positionClass: "toast-bottom-center" 
+};
